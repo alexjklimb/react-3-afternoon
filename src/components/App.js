860024642892f20c87c293cf3374f6e08bb19c45
alexjklimb <alexjklimb@gmail.com>
2020-06-23 10:42:52 -0800

    Project Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+
+
 
 class App extends Component {
   constructor() {
@@ -19,32 +22,52 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(res => {
+      console.log(res); 
+      this.setState({posts:res.data});
+      })
+    .catch(err => console.log(err));
+  }
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts/?id=${id}`, {text})
+    .then(res => this.setState({posts:res.data}))
+    .catch(err => console.log(err))
   }
 
-  updatePost() {
-  
+  deletePost(id) {
+    console.log(id)
+    axios.delete(`https://practiceapi.devmountain.com/api/posts/?id=${id}`)
+    .then(res => this.setState({posts:res.data}))
+    .catch(err => console.log(err))
   }
 
-  deletePost() {
+  createPost(text) {
 
-  }
-
-  createPost() {
-
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {text})
+    .then(res => this.setState({posts:res.data}))
+    .catch(err => console.log(err))
   }
 
   render() {
     const { posts } = this.state;
-
+    const mappedPosts = posts.map((item, i) => (
+    <Post 
+      key={i} 
+      id={item.id} 
+      updatePostFn={this.updatePost} 
+      deletePostFn={this.deletePost}
+      text={item.text} 
+      date={item.date}
+      />))
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {mappedPosts}
         </section>
       </div>
     );
